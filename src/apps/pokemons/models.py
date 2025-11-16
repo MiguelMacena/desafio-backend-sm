bfb9ee470d3f8fc6dcd34d43ba1c8bd150976abb
MiguelMacena <miguelmacena1610@gmail.com>
2025-11-16 14:52:09 -0300

    feat: implementa relacionamento manytomany entre Trainer e Pokemon usando tabela intermediaria

--- src/apps/pokemons/models.py
+++ src/apps/pokemons/models.py
@@ -1,3 +1,22 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    # representa o modelo de um Pokemon
+
+    nome = models.CharField(max_length=100, unique=True)
+    # dois pokemons não podem ter o mesmo nome
+
+    # dados que serão preenchidos pela PokeAPI
+
+    foto = models.URLField(null=True, blank=True)
+    altura = models.PositiveIntegerField(null=True, blank=True)
+    peso = models.PositiveIntegerField(null=True, blank=True)
+    # URLField valida se o texto tem formato de URL
+    # null e blank True porque quem preenche é a POKE API
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
--- src/apps/trainers/models.py
+++ src/apps/trainers/models.py
@@ -1,3 +1,40 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from apps.pokemons.models import Pokemon
+
+
+class Trainer(models.Model):
+    # representa o modelo de um treinador
+
+    nome = models.CharField(max_length=100)
+    idade = models.PositiveBigIntegerField()
+
+    pokemon = models.ManyToManyField(
+        # cria relacionamento ManyToMany entre treinador e Pokemons
+        "Pokemon",
+        through="TrainerPokemon",
+        # possibilita controlar a tabela criada pelo Django
+        related_name="treinadores",
+        # define como acessar os treinadores a partir de um Pokemon
+    )
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class TrainerPokemon(models.Model):
+
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    # cria uma relação entre treinador e pokemom
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    # cria uma relação dentre pokemon e treinador
+
+    class Meta:
+        unique_together = ("trainer", "pokemon")
+        # barra a possibilidade de 1 treinador ter o mesmo pokemon
+
+    def __str__(self):
+        return f"{self.trainer.nome} possui {self.pokemon.nome}"
